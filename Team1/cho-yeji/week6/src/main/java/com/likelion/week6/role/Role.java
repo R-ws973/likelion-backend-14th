b723ddefb19eb4ae5bb9f346e4775d70d54bd5b5
yeji-dev25 <jyjjj0329@gachon.ac.kr>
2026-05-07 16:56:02 +0900

    feat: week6 스프링부트 전환 및 첫 API 구현

---
+++ Team1/cho-yeji/week6/src/main/java/com/likelion/week6/role/Role.java
@@ -0,0 +1,43 @@
+package com.likelion.week6.role;
+
+import com.likelion.week6.policy.SubmissionPolicy;
+
+public abstract class Role {
+    private final String name;
+    private final String major;
+    private final int generation;
+    private final String part;
+
+    protected Role(String name, String major, int generation, String part) {
+        this.name = name;
+        this.major = major;
+        this.generation = generation;
+        this.part = part;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public int getGeneration() {
+        return generation;
+    }
+
+    public String getPart() {
+        return part;
+    }
+
+    protected abstract SubmissionPolicy getSubmissionPolicy();
+
+    public boolean canSubmit() {
+        return getSubmissionPolicy().canSubmit();
+    }
+
+    public abstract String getRoleName();
+
+    public abstract String getDetailInfo();
+}
